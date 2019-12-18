043c73ed3f358860c5d80f1522cdbddb55972390
Emily Bartman <frank@emwaves.org>
2019-12-17 18:36:21 -0700

    play audio

---
+++ index.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const player = require('play-sound')(opts = {})
+
+function playOurSong () {
+  player.play('./the_space_of_your_mind.mp3', function (err) {
+    if (err) console.error(err);
+  })
+}
+
+
+
+playOurSong()
+
+module.exports = {
+  playOurSong
+}
